4ff73a633cca7b73a6eed51331ac7310934f45d3
vederko-p <ski6a_23@mail.ru>
2021-07-25 08:54:37 +0300

    Реализовано представление результатов классификации

--- back propagation/neural_classification_test.py
+++ back propagation/neural_classification_test.py
@@ -4,50 +4,59 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 from model import empirical_risk, loss_function, a, u, sigm, neural_model
-from back_prop import back_propagation
+from back_prop import back_propagation, back_propagation_average, rms_propagation
+from back_prop import ada_delta, adam, nadam
 
 
 classification_data = pd.read_csv('data/classification_two_clusters.csv')
 class_val = classification_data.values
 
-X, Y = class_val[:, :2], class_val[:, -1:]
+X, Y = class_val[:, :2], class_val[:, 2:]
 
-# whj = np.array([[0.7, -0.2], [-0.8, 0.4], [0.3, 0.6]])
-# wmh = np.array([[0.4, -0.3, 0.8], [0.1, -0.3, -0.4]])
+methods = [[back_propagation, back_propagation_average],
+           [rms_propagation, ada_delta],
+           [adam, nadam]]
 
-# print(empirical_risk(loss_function, wjh, whm, X, y))
+params = [[(X, Y, 3, 2, 0.5, 0.1, 10**-10, empirical_risk, loss_function, a, u, sigm, 1),
+           (X, Y, 3, 2, 0.5, 0.1, 10**-10, empirical_risk, loss_function, a, u, sigm, 1)],
+          [(X, Y, 3, 2, 0.5, 0.5, 0.1, 10**-10, 10**-3, empirical_risk, loss_function, a, u, sigm, 1),
+           (X, Y, 3, 2, 0.5, 0.5, 1, 10**-10, 10**-3, empirical_risk, loss_function, a, u, sigm, 1)],
+          [(X, Y, 3, 2, 0.9, 0.999, 10**-8, 0.5, 0.1, 10**-10, empirical_risk, loss_function, a, u, sigm, 1),
+           (X, Y, 3, 2, 0.9, 0.999, 10**-8, 0.5, 0.1, 10**-10, empirical_risk, loss_function, a, u, sigm, 1)]]
 
-'''
-X0 = np.append(X, -np.ones(X.shape[0]).reshape(-1, 1), axis=1)
-whj = np.array([[0.7, -0.2, 0.2], [-0.8, 0.4, -0.3], [0.3, 0.6, 0.4]])
-wmh = np.array([[0.4, -0.3, 0.8, 0.7], [0.1, -0.3, -0.4, -0.3]])
-# print(empirical_risk(loss_function, whj, wmh, X0, Y, 1))
-# print(loss_function(whj, wmh, X0[0].reshape(-1, 1), Y[0].reshape(-1, 1), 1))
-# print(u(X0[0].reshape(-1, 1), sigm, whj, 1))
-# print(whj.dot(X0[0].reshape(-1, 1)))
-'''
+labels = [['Back Propagation', 'SAGProp (Stochastic Average Gradient)'],
+          ['RMSProp (Running Mean Square)', 'AdaDelta (Adaptive Learning Rate)'],
+          ['Adam (Adaptive Momentum)', 'Nadam (Nesterov-accelerated adaptive momentum)']]
 
-one = 1
-wh, wm = back_propagation(X, Y, 3, 1, 0.5, 0.1, 10**-10, empirical_risk, loss_function, a, u, sigm, one)
 
-x1 = np.array([[25], [25]])
-x2 = np.array([[4], [5]])
-res1 = neural_model(x1, sigm, wh, wm, one)
-res2 = neural_model(x2, sigm, wh, wm, one)
-
-print('Результаты:', res1, res2)
-print('Веса:')
-print(wh)
-print(wm)
-
+fig, axs = plt.subplots(3, 2)
+# Обучающая выборка
 x1, x2 = class_val[:, 1].reshape(-1, 1), class_val[:, 0].reshape(-1, 1)
-fig, ax = plt.subplots()
-ax.scatter(x1[:20], x2[:20])
-ax.scatter(x1[20:], x2[20:], color='orange')
+for i in range(axs.shape[0]):
+    for j in range(axs.shape[1]):
+        axs[i, j].scatter(x1[:20], x2[:20], label='Обучающая выборка')
+        axs[i, j].scatter(x1[20:], x2[20:], color='orange', label='Обучающая выборка')
+        axs[i, j].set_title(labels[i][j])
+# Тестовая выборка
 test_x = [np.array([[j], [j]]) for j in range(-5, 30, 2)]
-for i in test_x:
-    if neural_model(i, sigm, wh, wm, one) > 0.5:
-        ax.scatter(i[0,0], i[1,0], color='red', edgecolors='black', linewidths=0.5, s=50)
-    else:
-        ax.scatter(i[0,0], i[1,0], color='blue', edgecolors='black', linewidths=0.5, s=50)
+
+# Обучение
+weights = []
+for i in range(len(methods)):
+    w = []
+    for j in range(len(methods[0])):
+        print('Обучение методом ' + labels[i][j] + ' завершено')
+        w.append(methods[i][j](*params[i][j]))
+    weights.append(w)
+
+# Изображение
+for i in range(len(methods)):
+    for j in range(len(methods[0])):
+        for x in test_x:
+            res = neural_model(x, sigm, weights[i][j][0], weights[i][j][1], 1)
+            if res[0, 0] > res[1, 0]:
+                axs[i, j].scatter(x[0, 0], x[1, 0], color='red', edgecolors='black', linewidths=0.5, s=50)
+            else:
+                axs[i, j].scatter(x[0, 0], x[1, 0], color='blue', edgecolors='black', linewidths=0.5, s=50)
+plt.legend()
 plt.show()
