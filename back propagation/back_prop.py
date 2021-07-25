3ff0193d7aba5b1ab20dc4e6379dee8dda79ff42
vederko-p <ski6a_23@mail.ru>
2021-07-25 08:13:11 +0300

    Реализован Adam (Adaptive Momentum)

--- back propagation/back_prop.py
+++ back propagation/back_prop.py
@@ -282,3 +282,164 @@ def ada_delta(X0, Y, h, m, alpha, lam, eta, acc, eps, Q, L, a, u, sigm, one=0):
             break
     return whj0, wmh0
 
+
+# ---| Adam (Adaptive Momentum) |---
+def adam(X0, Y, h, m, gamma, alpha, eps, lam, eta, acc, Q, L, a, u, sigm, one=0):
+    '''
+    :param X: training sample - numpy array
+    :param Y: training sample - numpy array
+    :param h: Количество нейронов скрытого слоя
+    :param m: Количество нейронов выходного слоя
+    :param lam: the rate of forgetting - value in (0, 1)
+    :param eta: gradient step (learning rate) - value in R
+    :param acc: accuracy - value in (0, 1)
+    :param Q: empirical risk function
+    :param L: loss function
+    :param a: outer layer function
+    :param u: hidden layer function
+    :param sigm: sigma function
+    :param one: Включение в сеть минус единиц
+    '''
+    if one:
+        X = np.append(X0, -np.ones(X0.shape[0]).reshape(-1, 1), axis=1)
+    else:
+        X = X0.copy()
+    l, n = X.shape
+    whj0 = np.ones(h*n).reshape(h, n)
+    wmh0 = np.ones((h + one)*m).reshape(m, (h + one))
+    # Расчет всех градиентов
+    grads_wmh = np.array([])
+    grads_whj = np.array([])
+    for xi, yi in zip(X, Y):
+        x, y = xi.reshape(-1, 1).copy(), yi.reshape(-1, 1).copy()
+        ui = u(x, sigm, whj0, one)
+        ai = a(ui, sigm, wmh0)
+        eim = (ai - y)
+        sdf_m = sd(sigm, wmh0.dot(ui))
+        sdf_h = sd(sigm, whj0.dot(x))
+        eih = wmh0[:, :(-one or None)].transpose().dot(eim * sdf_m)
+        mesh_t, mesh_u = np.meshgrid(ui.flatten(), (eim * sdf_m).flatten())
+        grads_wmh = np.append(grads_wmh, mesh_t * mesh_u * eta)
+        mesh_t, mesh_u = np.meshgrid(xi.flatten(), (eih * sdf_h).flatten())
+        grads_whj = np.append(grads_whj, mesh_t * mesh_u * eta)
+    s_wmh = (grads_wmh.reshape(l, m, h + one).sum(axis=0))**2
+    s_whj = (grads_whj.reshape(l, h, n).sum(axis=0))**2
+    q0 = Q(L, whj0, wmh0, X, Y, one)
+    v_wmh, v_whj = 0, 0
+    k = 1
+    while k <= 10000:  # True
+        # Прямой ход
+        i = np.random.randint(0, l)
+        xi, yi = X[i].reshape(-1, 1), Y[i].reshape(-1, 1)
+        ui = u(xi, sigm, whj0, one)
+        ai = a(ui, sigm, wmh0)
+        eim = (ai - yi)
+        q1 = (1-lam)*q0 + lam*L(whj0, wmh0, xi, yi, one)
+        if True:  # (q1 - q0) >= acc*(1 + q1)
+            # Обратный ход
+            q0 = q1
+            sdf_m = sd(sigm, wmh0.dot(ui))
+            sdf_h = sd(sigm, whj0.dot(xi))
+            eih = wmh0[:, :(-one or None)].transpose().dot(eim * sdf_m)
+            # Градиентный шаг
+            # Веса выходного слоя
+            mesh_t, mesh_u = np.meshgrid(ui.flatten(), (eim * sdf_m).flatten())
+            lg = mesh_t * mesh_u
+            v_wmh = gamma*v_wmh + (1-gamma)*lg
+            vs_wmh = v_wmh*((1-gamma**k)**-1)
+            s_wmh = alpha*s_wmh + (1-alpha)*(lg**2)
+            ss_wmh = s_wmh*((1-alpha**k)**-1)
+            wmh0 -= eta*vs_wmh / (ss_wmh**0.5 + eps)
+            # Веса скрытого слоя
+            mesh_t, mesh_u = np.meshgrid(xi.flatten(), (eih * sdf_h).flatten())
+            lg = mesh_t * mesh_u
+            v_whj = gamma*v_whj + (1-gamma)*lg
+            vs_whj = v_whj*((1-gamma**k)**-1)
+            s_whj = alpha*s_whj + (1-alpha)*(lg**2)
+            ss_whj = s_whj*((1-alpha**k)**-1)
+            whj0 -= eta*vs_whj / (ss_whj**0.5 + eps)
+        else:
+            break
+        k += 1
+    return whj0, wmh0
+
+
+# ---| Nadam (Nesterov-accelerated adaptive momentum) |---
+def nadam(X0, Y, h, m, gamma, alpha, eps, lam, eta, acc, Q, L, a, u, sigm, one=0):
+    '''
+    :param X: training sample - numpy array
+    :param Y: training sample - numpy array
+    :param h: Количество нейронов скрытого слоя
+    :param m: Количество нейронов выходного слоя
+    :param lam: the rate of forgetting - value in (0, 1)
+    :param eta: gradient step (learning rate) - value in R
+    :param acc: accuracy - value in (0, 1)
+    :param Q: empirical risk function
+    :param L: loss function
+    :param a: outer layer function
+    :param u: hidden layer function
+    :param sigm: sigma function
+    :param one: Включение в сеть минус единиц
+    '''
+    if one:
+        X = np.append(X0, -np.ones(X0.shape[0]).reshape(-1, 1), axis=1)
+    else:
+        X = X0.copy()
+    l, n = X.shape
+    whj0 = np.ones(h*n).reshape(h, n)
+    wmh0 = np.ones((h + one)*m).reshape(m, (h + one))
+    # Расчет всех градиентов
+    grads_wmh = np.array([])
+    grads_whj = np.array([])
+    for xi, yi in zip(X, Y):
+        x, y = xi.reshape(-1, 1).copy(), yi.reshape(-1, 1).copy()
+        ui = u(x, sigm, whj0, one)
+        ai = a(ui, sigm, wmh0)
+        eim = (ai - y)
+        sdf_m = sd(sigm, wmh0.dot(ui))
+        sdf_h = sd(sigm, whj0.dot(x))
+        eih = wmh0[:, :(-one or None)].transpose().dot(eim * sdf_m)
+        mesh_t, mesh_u = np.meshgrid(ui.flatten(), (eim * sdf_m).flatten())
+        grads_wmh = np.append(grads_wmh, mesh_t * mesh_u * eta)
+        mesh_t, mesh_u = np.meshgrid(xi.flatten(), (eih * sdf_h).flatten())
+        grads_whj = np.append(grads_whj, mesh_t * mesh_u * eta)
+    s_wmh = (grads_wmh.reshape(l, m, h + one).sum(axis=0))**2
+    s_whj = (grads_whj.reshape(l, h, n).sum(axis=0))**2
+    q0 = Q(L, whj0, wmh0, X, Y, one)
+    v_wmh, v_whj = 0, 0
+    k = 1
+    while k <= 10000:  # True
+        # Прямой ход
+        i = np.random.randint(0, l)
+        xi, yi = X[i].reshape(-1, 1), Y[i].reshape(-1, 1)
+        ui = u(xi, sigm, whj0, one)
+        ai = a(ui, sigm, wmh0)
+        eim = (ai - yi)
+        q1 = (1-lam)*q0 + lam*L(whj0, wmh0, xi, yi, one)
+        if True:  # (q1 - q0) >= acc*(1 + q1)
+            # Обратный ход
+            q0 = q1
+            sdf_m = sd(sigm, wmh0.dot(ui))
+            sdf_h = sd(sigm, whj0.dot(xi))
+            eih = wmh0[:, :(-one or None)].transpose().dot(eim * sdf_m)
+            # Градиентный шаг
+            # Веса выходного слоя
+            mesh_t, mesh_u = np.meshgrid(ui.flatten(), (eim * sdf_m).flatten())
+            lg = mesh_t * mesh_u
+            v_wmh = gamma*v_wmh + (1-gamma)*lg
+            vs_wmh = v_wmh*((1-gamma**k)**-1)
+            s_wmh = alpha*s_wmh + (1-alpha)*(lg**2)
+            ss_wmh = s_wmh*((1-alpha**k)**-1)
+            wmh0 -= eta*vs_wmh / (ss_wmh**0.5 + eps)
+            # Веса скрытого слоя
+            mesh_t, mesh_u = np.meshgrid(xi.flatten(), (eih * sdf_h).flatten())
+            lg = mesh_t * mesh_u
+            v_whj = gamma*v_whj + (1-gamma)*lg
+            vs_whj = v_whj*((1-gamma**k)**-1)
+            s_whj = alpha*s_whj + (1-alpha)*(lg**2)
+            ss_whj = s_whj*((1-alpha**k)**-1)
+            whj0 -= eta*vs_whj / (ss_whj**0.5 + eps)
+        else:
+            break
+        k += 1
+    return whj0, wmh0
