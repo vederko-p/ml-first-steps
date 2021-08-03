275993c00c29574fe834100097dd3c0bfcb5906d
vederko-p <ski6a_23@mail.ru>
2021-08-04 00:32:05 +0300

    weighted kNN

--- lectures/metric methods/metric_classification_models.py
+++ lectures/metric methods/metric_classification_models.py
@@ -14,8 +14,8 @@ def gauss_core(r):
 def kNN(x, X, Y, k):
     # w(i, x):
     w = np.zeros(X.shape[0])
-    t = sorted(enumerate(cdist(x.reshape(-1, 2), X)[0]), key=lambda x: x[1])[:k]
-    u = np.array([i[0] for i in t])
+    t = sorted(enumerate(cdist(x.reshape(-1, 2), X)[0]), key=lambda x: x[1])[:k]  # sort by distance (+ indexes)
+    u = np.array([i[0] for i in t])  # indexes of k nearest neighbors
     w[u] = 1
     # [y(i) = y]:
     a = np.meshgrid(np.unique(Y), w)[0] == Y.reshape(-1, 1)
@@ -23,3 +23,19 @@ def kNN(x, X, Y, k):
     s = (a*w.reshape(-1, 1)).sum(axis=0)
     return np.argmax(s)
 
+
+# ---| Weighted kNN |---
+def weighted_kNN(x, X, Y, k, q):
+    # w(i, x):
+    w = np.zeros(X.shape[0])
+    t = sorted(enumerate(cdist(x.reshape(-1, 2), X)[0]), key=lambda x: x[1])[:k]  # sort by distance (+ indexes)
+    u = [i[0] for i in t]  # indexes of k nearest neighbors
+    for local_indx,indx in enumerate(u):
+        # local_indx - index of neighbor among k nearestneighbors
+        w[indx] = q**(local_indx+1)
+    # [y(i) = y]:
+    a = np.meshgrid(np.unique(Y), w)[0] == Y.reshape(-1, 1)
+    # sum([y(i) = y] w(i, x)):
+    s = (a*w.reshape(-1, 1)).sum(axis=0)
+    # return s
+    return np.argmax(s)
